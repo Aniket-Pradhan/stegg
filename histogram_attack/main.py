bff2f380d5b3e2f474b2671ecbcabdb03b99f501
Aniket Pradhan <aniket17133@iiitd.ac.in>
2019-10-12 23:10:50 +0530

    Merged the noise script in main.py

--- histogram_attack/main.py
+++ histogram_attack/main.py
@@ -1,16 +1,36 @@
 import argparse
+import skimage
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
 
 from hist_attack import histogram_attack
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--cover", required=True, help="Path to the cover image")
 ap.add_argument("-s", "--suspicious", required=True, help="Path to the suspicious image.")
+ap.add_argument("-n", "--noise", required=False, help="Add some noise to the image and then check.", action="store_true")
 
 args = vars(ap.parse_args())
 
 if __name__ == "__main__":
     cover_image_path = args["cover"]
     suspicious_image_path = args["suspicious"]
+    add_noise = args["noise"]
+
+    if add_noise:
+        img = skimage.io.imread(cover_image_path)/255.0
+        gimg = skimage.util.random_noise(img, mode="gaussian")
+        im2 = Image.fromarray((gimg*255).astype(np.uint8))
+
+        cover_image_path = cover_image_path.strip().split("/")
+        img_name = cover_image_path.pop(len(cover_image_path) - 1)
+        cover_image_path.append("noisy_image_" + str(img_name))
+        img_name = ""
+        for i in cover_image_path:
+            img_name += str(i) + "/"
+        cover_image_path = img_name[:len(img_name)-1]
+        im2.save(cover_image_path)
 
     hist_attack = histogram_attack(cover_image_path, suspicious_image_path)
     hist_attack.attack()
