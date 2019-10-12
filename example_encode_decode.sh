07ebc294cca0d02c7342a23528118b56410fc3cd
Aniket Pradhan <aniket17133@iiitd.ac.in>
2019-10-12 18:07:59 +0530

    Renamed scripts folder.

--- example_encode_decode.sh
+++ example_encode_decode.sh
@@ -2,10 +2,10 @@
 
 # encode
 echo "Encoding"
-python scripts/main.py -e -c images/download.png -s temp_secret
+python encoder_decoder/main.py -e -c images/download.png -s temp_secret
 echo "Encoded"
 
 # decode
 echo "Decoding"
-python scripts/main.py -d -i steg_image.png
+python encoder_decoder/main.py -d -i steg_image.png
 echo "Decoded"
