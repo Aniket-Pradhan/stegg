808e2a86ff2fa7916a793192b91a9790a10fcca3
Aniket Pradhan <aniket17133@iiitd.ac.in>
2019-11-15 23:48:40 +0530

    Fix psnr

--- psnr.py
+++ psnr.py
@@ -1,16 +1,16 @@
+import sys
 import cv2
 
-def comps(name1,name2):
-	im1 = cv2.imread(name1)
-	im2 = cv2.imread(name2)
+def comps(file1, file2):
+	im1 = cv2.imread(file1)
+	im2 = cv2.imread(file2)
 
-	psnr = cv2.psnr(im1,im2)
-	return (psnr)
+	psnr = cv2.PSNR(im1,im2)
+	return psnr
 
-#name1 and name2 are file name of images
-#for that directory for computing PSNR
-#anikhet please use your args function to do the magic
+file1 = sys.argv[1]
+file2 = sys.argv[2]
 
-out = comps(name1,name2)
-print(out)
+psnr = comps(file1, file2)
+print(psnr)
 
